e4df4b7406738f4a6420457ff69f00efb65e901f
Pankaj Sharma <ps12978@gmail.com>
2021-11-21 19:28:48 +0530

    Update 3. GraphRepresentationAdjacencyListDirected.cpp

--- Graphs/3. GraphRepresentationAdjacencyListDirected.cpp
+++ Graphs/3. GraphRepresentationAdjacencyListDirected.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main()
 {
-    int n, m;
-    cin >> n >> m;
+    int nodes, edges;
+    cin >> nodes >> edges;
     
-    vector<int> adj[n+1];
+    vector<int> adj[nodes+1];
     
-    for(int i = 0; i < m; i++)
+    for(int i = 0; i < edges; i++)
     {
-        int u, v;
+        int u, v; // u and v represents node 1 and node 2 which are connected by an edge.
         cin >> u >> v;
         
         adj[u].push_back(v);
